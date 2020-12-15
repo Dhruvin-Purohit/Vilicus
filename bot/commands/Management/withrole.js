765e65992c62a26e5919eaccf85b83b94f321904
unknown <paragdhruvincoc@gmail.com>
2020-12-15 13:34:45 +0530

    Added a withrole command

--- bot/commands/Management/withrole.js
+++ bot/commands/Management/withrole.js
@@ -2,10 +2,10 @@ const { Command } = require('discord-akairo')
 const dmd = require('discord-md-tags')
 const { MessageEmbed } = require('discord.js')
 
-module.exports = class WithoutRole extends Command {
+module.exports = class WithRole extends Command {
     constructor() {
-        super('withoutrole', {
-            aliases: ['withoutrole'],
+        super('withrole', {
+            aliases: ['withrole'],
             channel: 'guild',
             clientPermissions: ['SEND_MESSAGES', 'ADD_REACTIONS', 'USE_EXTERNAL_EMOJIS', 'EMBED_LINKS'],
             args: [
@@ -21,8 +21,8 @@ module.exports = class WithoutRole extends Command {
     }
     async exec(message, { role }) {
         const member = await message.guild.members.fetch()
-        let nope = member.filter(m => !m.roles.cache.has(role.id))
-        if(!nope.size > 0) return message.channel.send(new MessageEmbed({description: '😂 Everyone has that role!', color: role.hexColor}))
-        else return message.channel.send(new MessageEmbed({title: `List`, description: `${nope.map(m => `${dmd.bold `${m.user.tag} - `}${dmd.code `${m.id}`}`).join(`\n`)}\n\nThe above member(s) don't have the ${role} role`, footer: `requested by ${message.author.tag}`, color: role.hexColor}))
+        let nope = member.filter(m => m.roles.cache.has(role.id))
+        if(!nope.size > 0) return message.channel.send(new MessageEmbed({description: '😂 No one actually has that role', color: role.hexColor}))
+        else return message.channel.send(new MessageEmbed({title: `List`, description: `${nope.map(m => `${dmd.bold `${m.user.tag} - `}${dmd.code `${m.id}`}`).join(`\n`)}\n\nThe above member(s) have the ${role} role`, footer: `requested by ${message.author.tag}`, color: role.hexColor}))
     }
 }
