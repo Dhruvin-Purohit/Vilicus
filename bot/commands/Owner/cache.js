158b630e117e3bec8705faa92a14afd02f343846
unknown <paragdhruvincoc@gmail.com>
2020-12-15 14:53:53 +0530

    Added a cache Command

---
+++ bot/commands/Owner/cache.js
@@ -0,0 +1,29 @@
+const { Command } = require('discord-akairo');
+const emojis = require('../../utils/emojis.json')
+
+module.exports = class Cache extends Command {
+    constructor() {
+        super('cache', {
+            aliases: ['cache'],
+            typing: true,
+            clientPermissions: ['SEND_MESSAGES', 'ADD_REACTIONS', 'USE_EXTERNAL_EMOJIS'],
+            description: {
+                content: 'Caches a given guild or the current guild...',
+                usage: '[ guild ]'
+            },
+            ownerOnly: true,
+            args: [
+                {
+                    id: 'guild',
+                    match: 'content',
+                    type: 'guild',
+                    default: (message) => message.guild
+                }
+            ]
+        })
+    }
+    async exec(message, { guild }) {
+        await guild.members.fetch()
+        message.react(emojis.discord.CheckMark)
+    }
+}
